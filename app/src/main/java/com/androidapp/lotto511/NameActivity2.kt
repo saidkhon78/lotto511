1f8ab80b9504bbc132af5a4c51828cc5f2023bba
saidkhon <saidkhontursunkhujaev@gmail.com>
2021-05-11 09:40:51 +0900

    intial commit

---
+++ app/src/main/java/com/androidapp/lotto511/NameActivity2.kt
@@ -0,0 +1,11 @@
+package com.androidapp.lotto511
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NameActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_name2)
+    }
+}
